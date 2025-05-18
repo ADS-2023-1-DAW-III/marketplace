b31b7d12c3ed73fd70ebc90b85b0d466b6f6b630
lucasramallo <lucasdebrito.jd@gmail.com>
2025-05-18 15:34:32 -0300

    feat: abacatepay integration

---
+++ backend/src/api/controllers/abacate.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Post, HttpCode } from '@nestjs/common';
+import { AbacateService, BillingData } from 'src/infra/service/abacate.service';
+
+@Controller('abacate')
+export class AbacateController {
+  constructor(private readonly abacateService: AbacateService) {}
+
+  @Post('pessoa')
+  @HttpCode(200)
+  createPessoa(): Promise<void> {
+    return this.abacateService.createCustomer(
+      'João Lucas de Brito Ramalho Silva',
+      '(11) 4002-8922',
+      'lucasteste@gmail.com',
+      '220.002.720-61',
+    );
+  }
+
+  @Post('cobranca')
+  @HttpCode(200)
+  createCobranca(): Promise<BillingData | undefined> {
+    return this.abacateService.createBilling({
+      frequency: 'ONE_TIME',
+      methods: ['PIX'],
+      products: [
+        {
+          externalId: 'prod-1234',
+          name: 'Assinatura de Programa Fitness',
+          description: 'Acesso ao programa fitness premium por 1 mês.',
+          quantity: 2,
+          price: 2000,
+        },
+      ],
+      returnUrl: 'https://example.com/billing',
+      completionUrl: 'https://example.com/completion',
+      customerId: 'cust_GYSMaHMz4jG0JjyQpgTRyAgT',
+    });
+  }
+}
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PessoaModule } from './modules/pessoa/pessoa.module';
+import { AbacateController } from './api/controllers/abacate.controller';
+import { AbacateModule } from './infra/service/abacate.module';
 
 @Module({
-  imports: [PessoaModule],
+  imports: [PessoaModule, AbacateModule],
+  controllers: [AbacateController],
 })
 export class AppModule {}
