919ad17df0b47dcd008d3915f341f34b4811af32
lrickelme <rickelmelucas200906@gmail.com>
2025-05-24 17:29:48 -0300

    feat[categoria]: Criando o modulo da entidade categoria (#14)

---
+++ backend/src/modules/categoria/categoria.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, Column, PrimaryColumn, ManyToMany } from 'typeorm';
+import { Servico } from '../../servico/servico.entity';
+
+@Entity('categorias')
+export class Categoria {
+    @PrimaryColumn()
+    nome: string;
+
+    @Column({ type: 'text', nullable: true })
+    descricao?: string;
+
+    @ManyToMany(() => Servico, servico => servico.categorias)
+    servicos: Servico[];
+}
---
+++ backend/src/modules/categoria/dto/createCategoriaResponse.dto.ts
@@ -0,0 +1,11 @@
+import { Categoria } from '../categoria.entity';
+
+export class CreateCategoriaResponseDto {
+    nome: string;
+    descricao: string;
+
+    constructor(categoria: Categoria) {
+        this.nome = categoria.nome;
+        this.descricao = categoria.descricao;
+    }
+}
