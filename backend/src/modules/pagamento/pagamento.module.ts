80dbee7a7709e592029982abd0eb41fd81e2e853
dinizhugo <hugodiniztech01@gmail.com>
2025-05-24 22:38:53 -0300

    feat[module]: Adicionando o module de negociacao, pagamento e serviço(#14)

--- backend/src/modules/pagamento/pagamento.module.ts
+++ backend/src/modules/pagamento/pagamento.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../../infra/database.module';
+import { PagamentoProviders } from '../../infra/repositories/pagamento.providers';
+import { PagamentoService } from './pagamento.service';
+import { PagamentoController } from '../../api/controllers/pagamento.controller';
 
-@Module({})
+@Module({
+  imports: [DatabaseModule],
+  providers: [...PagamentoProviders, PagamentoService],
+  controllers: [PagamentoController],
+  exports: [PagamentoService],
+})
 export class PagamentoModule {}
--- backend/src/modules/servico/servico.module.ts
+++ backend/src/modules/servico/servico.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/infra/database.module';
+import { ServicoProviders } from 'src/infra/repositories/servico.providers';
+import { ServicoService } from './servico.service';
+import { ServicoController } from 'src/api/controllers/servico.controllers';
 
-@Module({})
+@Module({
+  imports: [DatabaseModule],
+  providers: [...ServicoProviders, ServicoService],
+  controllers: [ServicoController],
+  exports: [ServicoService],
+})
 export class ServicoModule {}
