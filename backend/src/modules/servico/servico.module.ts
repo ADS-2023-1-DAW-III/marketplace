0bd3e2aac81acef648fe9213e93a8acc6c741a28
lrickelme <rickelmelucas200906@gmail.com>
2025-05-25 15:41:21 -0300

    fix[servico.module]: Corrigindo o import de Serviço Controller após correção da nomeação do Controller para adequação aos padrões corretos de nomeação (#14)

--- backend/src/modules/servico/servico.module.ts
+++ backend/src/modules/servico/servico.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/infra/database.module';
 import { ServicoProviders } from 'src/infra/repositories/servico.providers';
 import { ServicoService } from './servico.service';
-import { ServicoController } from 'src/api/controllers/servico.controllers';
+import { ServicoController } from 'src/api/controllers/servico.controller';
 
 @Module({
   imports: [DatabaseModule],
