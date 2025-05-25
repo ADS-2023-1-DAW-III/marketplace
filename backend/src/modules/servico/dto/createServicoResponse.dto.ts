17beb0e46164d1f635e7380037746dcbd5791cf1
lrickelme <rickelmelucas200906@gmail.com>
2025-05-25 17:03:55 -0300

    feat[serviço]: Criando os DTOs da entidade Serviço (#14)

---
+++ backend/src/modules/servico/dto/createServicoResponse.dto.ts
@@ -0,0 +1,21 @@
+import { Servico } from '../servico.entity';
+
+export class ServicoResponseDto {
+    id: string;
+    caminhoImagem: string;
+    titulo: string;
+    eh_negociavel: boolean;
+    descricao: string;
+    preco: number;
+    duracao: number;
+
+    constructor(servico: Servico) {
+        this.id = servico.id;
+        this.caminhoImagem = servico.caminhoImagem;
+        this.titulo = servico.titulo;
+        this.eh_negociavel = servico.eh_negociavel;
+        this.descricao = servico.descricao;
+        this.preco = servico.preco;
+        this.duracao = servico.duracao;
+    }
+}
