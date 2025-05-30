90bde0a93d5552bd4ddd94490963477bec045d20
ismaellima4 <ismael.github@gmail.com>
2025-05-30 18:04:23 -0300

    chore[errorhandler]: adicionando error handling para erros de database

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,34 @@
-import { NestFactory } from '@nestjs/core';
+import { BaseExceptionFilter, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import {
+  ArgumentsHost,
+  BadRequestException,
+  Catch,
+  ValidationPipe,
+} from '@nestjs/common';
+import { QueryFailedError } from 'typeorm';
 
 dotenv.config();
 
+@Catch(QueryFailedError)
+export class QueryErrorFilter extends BaseExceptionFilter {
+  public catch(exception: any, host: ArgumentsHost): any {
+    const detail = exception.detail;
+    if (typeof detail === 'string' && detail.includes('already exists')) {
+      const messageStart = exception.table.split('_').join(' ') + ' with';
+      throw new BadRequestException(
+        exception.detail.replace('Key', messageStart),
+      );
+    }
+    return super.catch(exception, host);
+  }
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new QueryErrorFilter());
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
