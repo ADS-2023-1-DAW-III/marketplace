c4ac7b9324305ce3424cdf10d098027b301ec9cd
dinizhugo <hugodiniztech01@gmail.com>
2025-06-01 23:52:35 -0300

    feat[filters]: Criando o  modulo responsavel pelo filters(global exception handler) (#43)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      
+    })
+  )
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
