660aa3769c3480e77f0483c32d6f3c0fce357200
Isaac Alves Leal de Oliveira <isaacalves2406@gmail.com>
2025-06-03 22:16:53 -0300

    Falta somente a logica

--- frontend/app/pages/welcome/welcome.tsx
+++ frontend/app/pages/welcome/welcome.tsx
@@ -10,7 +10,7 @@ import {
 import camera from "~/assets/camera.png";
 import { useForm } from "react-hook-form";
 import { Input } from "~/components/ui/input";
-import { Button } from "~/components/ui/button";
+import upload from "~/assets/upload.png";
 
 export function DropdownSelect() {
   const options = [
@@ -28,16 +28,15 @@ export function Welcome() {
     <main className="flex min-h-screen">
       <div className="w-1/2 bg-white flex items-center justify-center">
         <div className="flex gap-4 items-center">
-          <span className="text-blue-500 text-5xl font-bold">M A R K E T</span>
-          <span className="bg-blue-300 text-white text-5xl font-bold px-1 py-0">
+          <span className="text-blue-500 text-7xl font-bold">M A R K E T</span>
+          <span className="bg-blue-400 text-white text-7xl font-bold px-1 py-0">
             PLACE
           </span>
         </div>
       </div>
 
-
       <div className="w-1/2 bg-blue-400 flex items-center justify-center">
-        <div className="bg-white rounded-xl shadow-lg p-8 w-full max-w-[450px]">
+        <div className="bg-white rounded-xl shadow-lg p-8 w-full max-w-[500px]">
           <Form {...form}>
             <form onSubmit={form.handleSubmit(() => console.log("enviado"))}>
               <FormField
@@ -45,11 +44,11 @@ export function Welcome() {
                 name="username"
                 render={({ field }) => (
                   <FormItem>
-                    <div className="flex items-center gap-4 mb-4">
+                    <div className="flex items-center gap-3 mb-2">
                       <div className="flex-1">
-                        <FormLabel>Nome</FormLabel>
+                        <FormLabel className="mb-2">Nome</FormLabel>
                         <FormControl>
-                          <Input className="w-full py-6 text-lg" {...field} />
+                          <Input className="w-full py-6 text-lg " {...field} />
                         </FormControl>
                       </div>
                       <div className="flex flex-col items-center justify-center">
@@ -60,34 +59,58 @@ export function Welcome() {
                             className="w-[30px] h-[30px] object-cover rounded-full"
                           />
                         </div>
-                        <span className="text-xs mt-1">Adicione foto</span>
+                        <span className="text-xs mt-1"><a href="">Adicone foto</a></span>
                       </div>
                     </div>
 
                     <FormLabel>Email</FormLabel>
                     <FormControl>
-                      <Input className="w-full py-6 text-lg" {...field} />
+                      <Input className="w-[435px] py-6 text-lg" {...field} />
                     </FormControl>
 
                     <FormLabel>Contato</FormLabel>
                     <FormControl>
-                      <Input className="w-full py-6 text-lg" {...field} />
+                      <Input className="w-[435px] py-6 text-lg" {...field} />
                     </FormControl>
 
                     <FormLabel>Senha</FormLabel>
                     <FormControl>
-                      <Input className="w-full py-6 text-lg" {...field} />
+                      <Input className="w-[435px] py-6 text-lg" {...field} />
                     </FormControl>
 
                     <FormLabel>Confirmar senha</FormLabel>
                     <FormControl>
-                      <Input className="w-full py-6 text-lg" {...field} />
+                      <Input className="w-[435px] py-6 text-lg" {...field} />
                     </FormControl>
+                      <FormLabel>Habilidades</FormLabel>
+                      <div className="flex items-end justify-between gap-1 mt-[-18px]">
+                        <FormControl>
+                          <select
+                            className="w-[220px] py-1 px-1 border rounded-md"
+                            {...form.register("habilidade")}
+                          >
+                            <option value="">Select</option>
+                            <option value="html">HTML</option>
+                            <option value="css">CSS</option>
+                            <option value="js">JavaScript</option>
+                            <option value="react">React</option>
+                          </select>
+                        </FormControl>
 
-                    <FormLabel>Habilidades</FormLabel>
-                    <FormControl>{/* Habilidades aqui */}</FormControl>
+                        <button
+                          type="submit"
+                          className="w-[150px] bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-5 rounded-md shadow-md transition duration-300 ease-in-out"
+                        >
+                          Criar Conta
+                        </button>
+                      </div>
 
-                    <FormMessage />
+
+                  <FormMessage />
+                    <div className="text-center mt-1">
+                      <span>Já possui uma conta? </span>
+                      <a href="/login" className="text-blue-600 hover:underline">Entrar</a>
+                    </div>
                   </FormItem>
                 )}
               />
