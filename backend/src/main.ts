c3074407fa6de4d0ec4e37f9851ee5f0d9665c9c
ismaellima4 <ismael.github@gmail.com>
2025-06-10 19:48:10 -0300

    feat[docs]: adiciona a config inicial do swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Marketplace')
+    .setDescription('sistema de Marketplace de Serviços')
+    .setVersion('1.0')
+    .addTag('Marketplace')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
