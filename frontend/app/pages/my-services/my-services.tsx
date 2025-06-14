7f278e04495546949d42dc2ec0bdb7b7286e9fc6
ramon <ramon.bento.oliver@gmail.com>
2025-06-14 19:39:34 -0300

    inicia tela de serviços prestados

---
+++ frontend/app/pages/my-services/my-services.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function MyServices() {
+  return (
+    <div className="max-w-4xl mx-auto p-6 font-poppins">
+      <div 
+        className="text-white p-4 rounded-lg shadow-md flex justify-center items-center min-h-[100px]" 
+        style={{ backgroundColor: '#307B8E' }}
+      >
+        <h2 className="text-xl font-semibold">Meus Serviços</h2>
+      </div>
+    </div>
+  );
+}
