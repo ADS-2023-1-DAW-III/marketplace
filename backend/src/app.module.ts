2cd279900e6bfa48ad70121ce81df6bf3df7fec5
lucasramallo <lucasdebrito.jd@gmail.com>
2025-07-18 21:14:32 -0300

    serveStaticOptions

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -30,6 +30,9 @@ import { join } from 'path';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'uploads'),
       serveRoot: '/uploads',
+      serveStaticOptions: {
+        index: false, 
+      },
     }),
   ],
   controllers: [
