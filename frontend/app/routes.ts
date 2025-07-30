78482be0d15de81ac306c2a397984913f826ae65
csabrina <ccosta.sabrina@gmail.com>
2025-07-30 20:37:55 -0300

    corrije esta bomba

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -12,13 +12,8 @@ export default [
       "historico_pagamento",
       "pages/historico_pagamento/HistoricoPagamento.tsx"
     ),
-    route("/perfil", "pages/perfil/Perfil.tsx"),
-    route(
-      "/servicos_prestados",
-      "pages/servicos_prestados/ServicosPrestados.tsx"
-    ),
     route("/service-registration", "pages/service/registration/ServiceRegistration.tsx"),
-    route("/perfil", "pages/perfil/Perfil.tsx"),
+        route("/perfil", "pages/perfil/Perfil.tsx"),
     route(
       "/servicos_prestados",
       "pages/servicos_prestados/ServicosPrestados.tsx"
