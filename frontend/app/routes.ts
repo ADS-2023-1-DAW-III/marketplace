a6afae9ac6ac69cdc500bb4cbb2a7c6565f7c528
csabrina <ccosta.sabrina@gmail.com>
2025-07-31 11:24:40 -0300

    refactor index

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,21 +1,21 @@
-// routes.ts
-import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
   layout("routes/LayoutAuth.tsx", [
     route("/login", "pages/auth/Login.tsx"),
     route("/register", "pages/auth/Register.tsx"),
   ]),
-  layout("routes/LayoutHome.tsx", [
-    route("/home", "pages/home/Home.tsx"),
+  layout("routes/index.tsx", [
+    index("pages/home/Home.tsx"),
     route("contratados", "pages/servicos_contratados/Contratados.tsx"),
     route(
       "historico_pagamento",
       "pages/historico_pagamento/HistoricoPagamento.tsx"
-    ),
-    route(
-      "/servicos_prestados/service-registration", 
-      "pages/service/registration/ServiceRegistration.tsx"
     ),
     route("/perfil", "pages/perfil/Perfil.tsx"),
     route(
@@ -23,4 +23,9 @@ export default [
       "pages/servicos_prestados/ServicosPrestados.tsx"
     ),
   ]),
+  route(
+    "/servicos_prestados/service-registration", 
+    "pages/service/registration/ServiceRegistration.tsx"
+  )
+  
 ] satisfies RouteConfig;
--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -1,10 +1,12 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import LayoutHome from "./LayoutHome";
 import type { MetaArgs } from "react-router";
 import { useNavigate } from "react-router";
 import { AuthContext } from "~/hooks/context/authContext";
+import { useApi } from "~/hooks/services/api";
+import type { AxiosError } from "axios";
 
-import ServiceRegistration from "~/pages/service/registration/ServiceRegistration";
+//import ServiceRegistration from "~/pages/service/registration/ServiceRegistration";
 
 export function meta(_args: MetaArgs) {
   return [
@@ -17,48 +19,47 @@ export default function Index() {
   const { token } = useContext(AuthContext);
   const navigate = useNavigate();
 
+  const api = useApi();
+  const [loading, setLoading] = useState(true);
+
+  const heathly = async () => {
+    try {
+      const response = await api.get("/servicos");
+      if (response.status === 401) {
+        throw new Error("Unauthorized");
+      }
+      return [200, 202, 204].includes(response.status);
+    } catch (error) {
+      console.error("Erro ao verificar a saúde do serviço:", error);
+      return false;
+    }
+  };
+
+  const handleHealthy = () => {
+    setLoading(false);
+  };
+
+  const handleUnhealthy = () => {
+    navigate("/login", { replace: true });
+  };
+
   useEffect(() => {
     if (!token) {
       navigate("/login", { replace: true });
+      } else {
+      heathly()
+        .then((isHealthy) => {
+          if (isHealthy) {
+            handleHealthy();
+          } else {
+            throw new Error("Serviço indisponível");
+          }
+        })
+        .catch((error: AxiosError) => {
+          handleUnhealthy();
+        });
     }
   }, [token]);
 
-  return token && <LayoutHome />;
+  return token && !loading && <LayoutHome />;
 }
-
-const authLoader = () => {
-  const token = localStorage.getItem('token');
-  if (!token) {
-    throw new Response(null, {
-      status: 302,
-      headers: {
-        Location: '/login',
-      },
-    });
-  }
-  return null;
-};
-
-export const routes = [
-  {
-    path: "/",
-    element: <Index />,
-  },
-  {
-    path: "/servicos_prestados/service-registration",
-    element: <ServiceRegistration />,
-    loader: authLoader,
-  },
-  {
-    path: "/service-registration",
-    loader: () => {
-      // Redireciona para a nova rota
-      throw new Response(null, {
-        status: 301,
-        headers: {
-          Location: '/servicos_prestados/service-registration',
-        },
-      });
-    },
-  },
-];
